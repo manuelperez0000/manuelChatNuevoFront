d3eade2a9bd018760002ffd4751aaaf262e5a2d2
manuel perez <manuelperez.0000@gmail.com>
2025-06-19 12:12:15 -0400

    Loading corregido

--- src/pages/LobbyPage.jsx
+++ src/pages/LobbyPage.jsx
@@ -1,6 +1,6 @@
 import useLobby from "../hooks/useLobby"
 import avatarImages from "../avatars/importers"
-import Loading from "../components/Loading"
+import Loading from "../components/loading"
 
 export default function Home() {
 
